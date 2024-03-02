dea698e56217cc760b7a5cb0e0f812bd06486d51
YuchenPan1228 <ypan40@stuy.edu>
2024-03-02 16:41:17 -0500

    update

--- app/kol-recruitment/page.tsx
+++ app/kol-recruitment/page.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+'use client'
 
+import React, { useState } from 'react';
 
 const KolRecruitmentPage: React.FC = () => {
     const [formData, setFormData] = useState({
